1f7d4365f1dbceaccd9783551a44144d94fcdc57
Toha Hossain <tohahpro@gmail.com>
2025-07-14 11:14:17 +0600

    Set the accessToken and refreshToken in Cookies

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import cors from "cors"
 import { router } from './app/routes';
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
 import { notFound } from './app/middlewares/notFound';
+import cookieParser from 'cookie-parser';
 
 
 const app : Application = express();
 
+app.use(cookieParser())
 app.use(express.json())
- app.use(cors())
+app.use(cors())
 
 app.use("/api/v1", router) 
 
--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,10 +4,22 @@ import { NextFunction, Request, Response } from "express"
 import { catchAsync } from "../../utils/catchAsync"
 import { sendResponse } from '../../utils/sendResponse';
 import { AuthServices } from './auth.services';
+import AppError from '../../errorHandlers/AppError';
 
 const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction)=>{
     const loginInfo = await AuthServices.credentialsLogin(req.body)
 
+    // set accessToken in cookies 
+    res.cookie('accessToken', loginInfo.accessToken, {
+        httpOnly: true,
+        secure: false
+    })
+
+    // set refreshToken in cookies 
+    res.cookie('refreshToken', loginInfo.refreshToken, {
+        httpOnly: true,
+        secure: false
+    })
     sendResponse(res,{
         success: true,
         statusCode: httpStatus.OK,
@@ -18,6 +30,9 @@ const credentialsLogin = catchAsync(async (req: Request, res: Response, next: Ne
 
 const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction)=>{
     const refreshToken = req.cookies.refreshToken
+    if(!refreshToken){
+        throw new AppError(httpStatus.BAD_REQUEST, "Not refresh token received from cookies")
+    }
     const tokenInfo = await AuthServices.getNewAccessToken(refreshToken)
 
     sendResponse(res,{
