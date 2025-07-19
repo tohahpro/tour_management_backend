b5940d421322aec2e2a0f0445a3ad7426418ac3d
Toha Hossain <tohahpro@gmail.com>
2025-07-19 15:46:36 +0600

    Handling Mongoose Validation Error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,13 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response} from 'express';
 import { envVars } from "../config/env"
 import AppError from '../errorHandlers/AppError';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const globalErrorHandler = (err:any, req: Request, res:Response, next: NextFunction)=>{
     
     let statusCode = 500
     let message= `something went wrong!!`
+
+        const errorSources= [
+            // {
+            //     path: "isDeleted",
+            //     message: "Cast Failed"
+            // }
+        ]
 
     // Duplicate 
     if(err.code === 11000){
@@ -19,6 +26,16 @@ export const globalErrorHandler = (err:any, req: Request, res:Response, next: Ne
     else if(err.name === 'CastError'){
         statusCode = 500;
         message = "Invalid mongoDB ObjectId. Please provide a valid id"
+    }
+    else if (err.name === "ValidationError") {
+        statusCode = 400;
+        const errors = Object.values(err.errors);
+
+        errors.forEach((errorObject: any) => errorSources.push({
+            path: errorObject.path,
+            message: errorObject.message
+        }));
+        message = "Validation error occurred";
     }
     else if(err instanceof AppError){
         statusCode = err.statusCode
