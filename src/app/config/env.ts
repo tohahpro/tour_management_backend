0e0857bd754df802ffacf3be2c7c2b425f713713
Toha Hossain <tohahpro@gmail.com>
2025-07-25 18:17:57 +0600

    SSL Commerz , Account Creation Process

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,75 +1,107 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
+dotenv.config();
 
-dotenv.config()
+interface EnvConfig {
+    PORT: string;
+    DB_URL: string;
+    NODE_ENV: string;
+    BCRYPT_SALT_ROUND: string;
 
-interface EnvConfig{
-    PORT: string
-    DB_URL: string
-    NODE_ENV: string
-    BCRYPT_SALT_ROUND: string
+    JWT_ACCESS_SECRET: string;
+    JWT_ACCESS_EXPIRES: string;
 
-    JWT_ACCESS_SECRET: string
-    JWT_ACCESS_EXPIRES: string
+    JWT_REFRESH_SECRET: string;
+    JWT_REFRESH_EXPIRES: string;
 
-    JWT_REFRESH_SECRET: string
-    JWT_REFRESH_EXPIRES: string
+    SUPER_ADMIN_EMAIL: string;
+    SUPPER_ADMIN_PASSWORD: string;
 
-    SUPER_ADMIN_EMAIL: string
-    SUPPER_ADMIN_PASSWORD: string
-
-
-    GOOGLE_CLIENT_SECRET: string
-    GOOGLE_CLIENT_ID: string
-    GOOGLE_CALLBACK_URL: string
-    EXPRESS_SESSION_SECRET: string
-    FRONTEND_URL: string
+    GOOGLE_CLIENT_SECRET: string;
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CALLBACK_URL: string;
+    EXPRESS_SESSION_SECRET: string;
+    FRONTEND_URL: string;
+    SSL: {
+        STORE_ID: string;
+        STORE_PASS: string;
+        SSL_PAYMENT_API: string;
+        SSL_VALIDATION_API: string;
+        SSL_SUCCESS_FRONTEND_URL: string,
+        SSL_FAIL_FRONTEND_URL: string,
+        SSL_CANCEL_FRONTEND_URL: string,
+        SSL_SUCCESS_BACKEND_URL: string,
+        SSL_FAIL_BACKEND_URL: string,
+        SSL_CANCEL_BACKEND_URL: string,
+    };
 }
 
-const loadEnvVariables = (): EnvConfig =>{
-
+const loadEnvVariables = (): EnvConfig => {
     const requiredEnvVariables: string[] = [
-        "PORT", 
-        "DB_URL", 
-        "NODE_ENV", 
-        "BCRYPT_SALT_ROUND", 
-        "JWT_ACCESS_SECRET", 
-        "JWT_ACCESS_EXPIRES", 
-        "SUPPER_ADMIN_PASSWORD", 
+        "PORT",
+        "DB_URL",
+        "NODE_ENV",
+        "BCRYPT_SALT_ROUND",
+        "JWT_ACCESS_SECRET",
+        "JWT_ACCESS_EXPIRES",
+        "SUPPER_ADMIN_PASSWORD",
         "SUPER_ADMIN_EMAIL",
         "JWT_REFRESH_SECRET",
         "JWT_REFRESH_EXPIRES",
-        'GOOGLE_CLIENT_SECRET',
-        'GOOGLE_CLIENT_ID',
-        'GOOGLE_CALLBACK_URL',
-        'EXPRESS_SESSION_SECRET',
-        'FRONTEND_URL'
+        "GOOGLE_CLIENT_SECRET",
+        "GOOGLE_CLIENT_ID",
+        "GOOGLE_CALLBACK_URL",
+        "EXPRESS_SESSION_SECRET",
+        "FRONTEND_URL",
+        "SSL_STORE_ID",
+        "SSL_STORE_PASS",
+        "SSL_PAYMENT_API",
+        "SSL_VALIDATION_API",
+        "SSL_SUCCESS_FRONTEND_URL",
+        "SSL_FAIL_FRONTEND_URL",
+        "SSL_CANCEL_FRONTEND_URL",
+        "SSL_SUCCESS_BACKEND_URL",
+        "SSL_FAIL_BACKEND_URL",
+        "SSL_CANCEL_BACKEND_URL"
     ];
 
-    requiredEnvVariables.forEach(key=>{
-        if(!process.env[key]){
-            throw new Error(`Missing require environment variable ${key}`)
+    requiredEnvVariables.forEach((key) => {
+        if (!process.env[key]) {
+            throw new Error(`Missing require environment variable ${key}`);
         }
-    })
+    });
 
     return {
         PORT: process.env.PORT as string,
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         DB_URL: process.env.DB_URL!,
-        NODE_ENV: process.env.NODE_ENV as 'development' | 'production',
+        NODE_ENV: process.env.NODE_ENV as "development" | "production",
         BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
         JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
         JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
         JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
-        JWT_REFRESH_EXPIRES:process.env.JWT_REFRESH_EXPIRES as string,
+        JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
         SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
         SUPPER_ADMIN_PASSWORD: process.env.SUPPER_ADMIN_PASSWORD as string,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
         GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
         EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
-        FRONTEND_URL: process.env.FRONTEND_URL as string
-    }   
-}
+        FRONTEND_URL: process.env.FRONTEND_URL as string,
+        // SSL 
+        SSL: {
+            STORE_ID: process.env.SSL_STORE_ID as string,
+            STORE_PASS: process.env.SSL_STORE_PASS as string,
+            SSL_PAYMENT_API: process.env.SSL_PAYMENT_API as string,
+            SSL_SUCCESS_FRONTEND_URL: process.env.SSL_SUCCESS_FRONTEND_URL as string,
+            SSL_FAIL_FRONTEND_URL: process.env.SSL_FAIL_FRONTEND_URL as string,
+            SSL_CANCEL_FRONTEND_URL: process.env.SSL_CANCEL_FRONTEND_URL as string,
+            SSL_SUCCESS_BACKEND_URL: process.env.SSL_SUCCESS_BACKEND_URL as string,
+            SSL_FAIL_BACKEND_URL: process.env.SSL_FAIL_BACKEND_URL as string,
+            SSL_CANCEL_BACKEND_URL: process.env.SSL_CANCEL_BACKEND_URL as string,
+            SSL_VALIDATION_API: process.env.SSL_VALIDATION_API as string,
+        },
+    };
+};
 
-export const envVars = loadEnvVariables()
+export const envVars = loadEnvVariables();
--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import httpStatus from 'http-status-codes';
 import AppError from "../../errorHandlers/AppError";
 import { User } from "../user/user.model";
@@ -6,52 +7,86 @@ import { Booking } from './booking.model';
 import { Payment } from '../payment/payment.model';
 import { PAYMENT_STATUS } from '../payment/payment.interface';
 import { Tour } from '../tour/tour.model';
+import { sslService } from '../sslCommerz/sslCommerz.service';
+import { ISSLCommerz } from '../sslCommerz/sslCommerz.interface';
 
-const getTransactionId = () =>{
-    return `tran_${Date.now()}_${Math.floor(Math.random()* 1000)}`
+const getTransactionId = () => {
+    return `tran_${Date.now()}_${Math.floor(Math.random() * 1000)}`
 }
 const createBooking = async (payload: Partial<IBooking>, userId: string) => {
     const transactionId = getTransactionId()
 
-    const user = await User.findById(userId)
+    const session = await Booking.startSession()
+    session.startTransaction()
 
-    if(!user?.phone || !user.address){
-        throw new AppError(httpStatus.BAD_REQUEST, "Please Update Your Profile to Book a Tour.")
+    try {
+        const user = await User.findById(userId)
+
+        if (!user?.phone || !user.address) {
+            throw new AppError(httpStatus.BAD_REQUEST, "Please Update Your Profile to Book a Tour.")
+        }
+
+        const tour = await Tour.findById(payload.tour).select("costFrom")
+
+        if (!tour?.costFrom) {
+            throw new AppError(httpStatus.BAD_REQUEST, "No Tour Cost Found")
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const amount = Number(tour.costFrom) * Number(payload.guestCount!)
+
+        const booking = await Booking.create([{
+            user: userId,
+            status: Booking_Status.PENDING,
+            ...payload
+        }],{session})
+
+        const payment = await Payment.create([{
+            booking: booking[0]._id,
+            status: PAYMENT_STATUS.UNPAID,
+            transactionId: transactionId,
+            amount: amount
+        }],{session})
+
+        const updatedBooking = await Booking
+            .findByIdAndUpdate(
+                booking[0]._id,
+                { payment: payment[0]._id },
+                { new: true, runValidators: true, session }
+            )
+            .populate("user", "name email phone address")
+            .populate("tour", "title costFrom")
+            .populate("payment") // user, tour, payment is field name
+        
+        const userAddress = (updatedBooking?.user as any).address
+        const userEmail = (updatedBooking?.user as any).email
+        const userPhoneNumber = (updatedBooking?.user as any).phone
+        const userName = (updatedBooking?.user as any).name
+
+        const sslPayload: ISSLCommerz={
+            address: userAddress,
+            email: userEmail,
+            phoneNumber: userPhoneNumber,
+            name: userName,
+            amount: amount,
+            transactionId: transactionId
+        }
+
+        const sslPayment = await sslService.sslPaymentInit(sslPayload)
+
+        await session.commitTransaction(); // transaction
+        session.endSession()
+        return {
+            paymentUrl: sslPayment.GatewayPageURL,
+            booking: updatedBooking
+        }
+    } 
+    catch (error: any) {
+        await session.abortTransaction() // rollback
+        session.endSession()
+        throw error
+
     }
-
-    const tour = await Tour.findById(payload.tour).select("costFrom")
-
-    if(!tour?.costFrom){
-        throw new AppError(httpStatus.BAD_REQUEST, "No Tour Cost Found")
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const amount = Number(tour.costFrom) * Number(payload.guestCount!)
-
-    const booking = await Booking.create({
-        user: userId,
-        status: Booking_Status.PENDING,
-        ...payload
-    })
-
-    const payment = await Payment.create({
-        booking: booking._id,
-        status: PAYMENT_STATUS.UNPAID,
-        transactionId: transactionId,
-        amount: amount
-    })
-
-    const updatedBooking = await Booking
-        .findByIdAndUpdate(
-            booking._id, 
-            {payment: payment._id},
-            {new: true, runValidators: true}
-        )
-        .populate("user", "name email phone address")
-        .populate("tour", "title costFrom")
-        .populate("payment") // user, tour, payment is field name
-
-    return updatedBooking
 
 };
 
